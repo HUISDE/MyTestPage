9f7b3380a54cd7f27d1426b1e1c6b7357d94828d
HUISDE <134346756+HUISDE@users.noreply.github.com>
2026-07-28 09:16:45 +0800

    Update config.js
    update config api

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@
  */
 const CONFIG = {
   // API 接口基地址（部署时修改此项）
-  API_BASE_URL: 'https://frp-fun.com:11302/api',
+  API_BASE_URL: 'https://frp-bar.com:37634/api',
 
   // Token 在 localStorage 中的存储键名
   TOKEN_KEY: 'trademark_token',
