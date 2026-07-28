9281d0c17627e173f6fa8ef770e3c2d571f9560c
HUISDE <134346756+HUISDE@users.noreply.github.com>
2026-07-28 14:59:24 +0800

    Update config.js
    update api config

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@
  */
 const CONFIG = {
   // API 接口基地址（部署时修改此项）
-  API_BASE_URL: 'https://frp-bar.com:37634/api',
+  API_BASE_URL: 'http://114.247.84.117:8086/api',
 
   
   // Token 在 localStorage 中的存储键名
